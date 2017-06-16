33801e92389ab5b81e1d4d38b9412dd89d6b87a7
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-06-16 15:32:32 -0500

    added names to loops

--- scripts.js
+++ scripts.js
@@ -1,21 +1,13 @@
-var lines;
-for (counter = 99; counter >= 1; counter = counter - 1) 
-{
-    if (counter == 1) {
-        lines = 'line';
-    } else {
-        lines = 'lines';
-    }
-    console.log(counter+" "+lines+" of code in the file.");
-    if (counter < 99) {
-        console.log("");
-        console.log(counter+" "+lines+" of code in the file.");
-    }
-    console.log(counter+" "+lines+" of code.");
-    console.log("strikes one out.");
-    console.log("clears it all out.");
-    if (counter == 1) {
-        console.log("No more lines of code in the file.");
+var friends = [ "Shala", "Merissa", "Steph", "Andre", "Alex" ];
+for(var i = 0; i < friends.length; i++) {
+
+   for(var j = 99; j >= 1; j--) {
+        if (j > 2) {
+            console.log([j] + ' lines of code in the file, ' + [j] + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j-1) + ' lines of code in the file' );
+        } else if (j == 2) {
+            console.log([j] + ' lines of code in the file, ' + [j] + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j-1) + ' line of code in the file' );
+        } else if (j = 1) {
+            console.log([j] + ' line of code in the file, ' + [j] + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file' );
+        }
     }
 }
- 
